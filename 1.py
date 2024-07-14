7b8fe6d66348aa2ec3188a1790c0551e1559a31d
Alex-batraholog <alexvasvas78@gmail.com>
2024-07-14 17:06:48 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+#'1st programm'
+print((9**0.5)*5)
+#'2nd programm'
+print(9.99>9.98 and 1000!=1000.1)
+#'3rd programm'
+print(((1234%1000)//10)+((5678%5000)//10))
+#'4th programm'
+print(13.42//1)
+
